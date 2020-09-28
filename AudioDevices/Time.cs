9b4821dc007c39f32107f900a23e54bdf7686c30
Thijs van Dorp <Thijsjuh97@hotmail.com>
2020-09-28 14:41:36 +0200

    Init

---
+++ AudioDevices/Time.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AudioDevices
+{
+    class Time
+    {
+        int _hours;
+        int _minutes;
+        int _seconds;
+        public Time(int seconds)
+        {
+         
+        }
+        public Time(int minutes, int seconds)
+        {
+            _hours = minutes / 60;
+            _minutes = minutes % 60;
+            _seconds = seconds;
+        }
+        public Time (int hours, int minutes, int seconds)
+        {
+            _hours = hours;
+            _minutes = minutes;
+            _seconds = seconds;
+        }
+    }
+}
---
+++ AudioDevices/Track.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AudioDevices
+{
+    public class Track
+    {
+        private int id;
+        private string name;
+        private string artist;
+        private string albumSource;
+        private category style;
+        private Time length;
+    }
+    public enum category
+    {
+        Ambient,
+        Blues,
+        Country,
+        Disco,
+        Electro,
+        Hardcore,
+        HardRock,
+        HeavyMetal,
+        Hiphop,
+        Jazz,
+        Jumpstyle,
+        Klassiek,
+        Latin,
+        Other,
+        Pop,
+        Punk,
+        Reggae,
+        Rock,
+        Soul,
+        Trance,
+        Techno
+    }
+}
