407ddbf58f3472dee727dbe3c8ad07b2a9eed47a
Thijs van Dorp <Thijsjuh97@hotmail.com>
2020-10-21 14:37:56 +0200

    Add comments in Time and Track for further improvements.

--- AudioDevices/Time.cs
+++ AudioDevices/Time.cs
@@ -6,25 +6,32 @@ namespace AudioDevices
 {
     public class Time
     {
+                    //Maak 3 lokale variabelen aan
         int _hours;
         int _minutes;
         int _seconds;
+                    //Maak 3 constructors aan die de tijd berekenen.
         public Time(int seconds)
         {
+                    //Hier worden seconden omgerekent naar minuten en uren.
             _hours = seconds / 3600;
             _minutes = (seconds % 3600) / 60;
             _seconds = (seconds % 3600) % 60;
         }
+                    //In deze constructor worden minuten en seconden omgerekend.
         public Time(int minutes, int seconds) : this(minutes * 60 + seconds)
         {
 
         }
+                    //In deze constructor worden uren, minuten en seconden omgerekend.
         public Time (int hours, int minutes, int seconds) : this(hours * 3600 + minutes * 60 + seconds)
         {
     
         }
         public override string ToString()
         {
+                    //Als de waardes van uren, minuten en seconden minder dan 10
+                    //is zorg dan dat er een extra 0 voor het cijfer komt te staan zodat het netjes double digits is.
             string result = "";
             result += _hours < 10 ? "0" + _hours : _hours.ToString();
             result += ":";
--- AudioDevices/Tracks.cs
+++ AudioDevices/Tracks.cs
@@ -4,14 +4,14 @@ namespace AudioDevices
 {
     public class Tracks
     {
-        // Initialize private fields
+                    // Initialize private fields
         private int id;
         private string name;
         private string artist;
         private string albumSource;
         private Category style;
         private Time length;
-
+                    //Vul de private fields dmv get en setters.
         public int Id { get { return id; } set { id = value; } }
         public string Name { get { return name; } set { name = value; } }
         public string Artist { get { return artist; } set { artist = value; } }
@@ -19,17 +19,19 @@ namespace AudioDevices
         {
             get
             {
+                            //Als de Artiest of Naam onbekend is, geef een string genaamd "Unknown" terug.
                 if (String.IsNullOrEmpty(artist) || String.IsNullOrEmpty(name))
                 {
                     return "Unknown";
                 }
                 else
+                            //Als de Artiest en Naam bekend zijn, geef dan de waardes mee.
                 {
                     return artist + name;
                 }
             }
         }
-        //Definieer Get en Setters voor Private fields
+                    //Definieer Get en Setters voor Private fields
         public Time Length { get { return length; } set { length = value ; } }
         public string DisplayLength { get { return this.length.ToString(); } }
         public Category Style { get { return style; } set { style = value; } }
@@ -40,11 +42,15 @@ namespace AudioDevices
         }
         public int GetLengthInSeconds()
         {
-            //uur:minuut:seconden
+                        //De string komt terug in een xx:xx:xx format.
+                        //er word een Split functie om dit te splitsen in Uren, Minuten en seconden.
             string[] time = length.ToString().Split(':');
+                        //Converteer de gesplitste waardes weer terug naar Int waardes en reken deze om naar seconden (1uur = 3600sec, 1min = 60sec).
             int seconds = (Convert.ToInt32(time[0]) * 3600) + (Convert.ToInt32(time[1]) * 60) + (Convert.ToInt32(time[2]));
+                        //Return de berkende seconden.
             return GetLengthInSeconds();
         }
+        //Maak 4 constructors aan.
         public Tracks() { }
         public Tracks(int id) {
             this.id = id;
