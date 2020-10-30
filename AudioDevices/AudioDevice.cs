aedc49a832562002f84f36ed3633174be59e0623
Thijs <Thijsjuh97@hotmail.com>
2020-10-30 11:01:18 +0100

    Added Audiodevice.cs

---
+++ AudioDevices/AudioDevice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioDevices.Devices
+{
+    abstract class AudioDevice
+    {
+        private int serialId;
+        private string model;
+        private string make;
+        private decimal priceExBtw;
+        private DateTime creationDate;
+        protected readonly static double btwPercentage = 19.0;
+        public string DisplayIdentity()
+        {
+            string text = "Serial: " + serialId;
+            return text;
+        }
+        public string DisplayIdentity(bool makeInfo, bool modelInfo)
+        {
+            return DisplayIdentity();
+        }
+    }
+}
