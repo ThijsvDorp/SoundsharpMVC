5f1cef927539bd7c1762837f7ab3aafcf5f91a4f
Thijs <Thijsjuh97@hotmail.com>
2020-11-03 14:20:30 +0100

    Added 2 methods, Displayidentity()

--- AudioDevices/AudioDevice.cs
+++ AudioDevices/AudioDevice.cs
@@ -6,22 +6,41 @@ using System.Threading.Tasks;
 
 namespace AudioDevices.Devices
 {
+    //Maak een abstracte class aan, een abstracte class kan niet geinstantieerd worden.
     abstract class AudioDevice
     {
+        //Maak een aantal private fields aan.
         private int serialId;
         private string model;
         private string make;
         private decimal priceExBtw;
         private DateTime creationDate;
         protected readonly static double btwPercentage = 19.0;
+        //Maak een method aan die de Serial ID teruggeeft.
         public string DisplayIdentity()
         {
             string text = "Serial: " + serialId;
             return text;
         }
+        //Overload deze method met 2 parameters die kijken of
         public string DisplayIdentity(bool makeInfo, bool modelInfo)
         {
-            return DisplayIdentity();
+            string text = "Serial: " + serialId;
+            //Als makeInfo true is, voeg een string toe die Make laat zien.
+            if (makeInfo == true)
+            {
+                text += " Make:" + make;
+            }
+            //Als modelInfo true is, voeg een string toe die Model laat zien.
+            if (modelInfo == true)
+            {
+                text += " Model:" + model;
+            }
+            return text;
+        }
+        public string GetDeviceLifeTime()
+        {
+            return "";
         }
     }
 }
