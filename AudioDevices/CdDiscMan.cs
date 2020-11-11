6a23259e5fb2cd165ab2d42f5f64a943f31962f5
Thijs van Dorp <Thijsjuh97@hotmail.com>
2020-11-11 14:03:56 +0100

    Added interfaces.cs

--- AudioDevices/CdDiscMan.cs
+++ AudioDevices/CdDiscMan.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static AudioDevices.Devices.MemoRecorder;
 
 namespace AudioDevices.Devices
 {
     class CdDiscMan : AudioDevice
     {
-      
+        public override string DisplayStorageCapacity()
+        {
+            throw new NotImplementedException("Not done yet");
+        }
+        private readonly int mBSize;
+        private int displayWidth;
+        private int displayHeight;
+        private bool isEjected = false;
     }
 }
---
+++ AudioDevices/Interfaces.cs
@@ -0,0 +1,20 @@
+﻿using AudioDevices.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioDevices.Interfaces
+{
+    class Interfaces
+    {
+        interface IDisplay
+        {
+            string GetResolutionInfo();
+            int DisplayWidth { get; set; }
+            int DisplayHeight { get; set; }
+            int TotalPixels { get; }
+        }
+    }
+}
